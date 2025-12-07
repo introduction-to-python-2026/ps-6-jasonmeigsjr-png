7017d6aa7ee84ebf4dc689533103a409f899670a
jasonmeigsjr-png <jasonmeigsjr@gmail.com>
2025-12-07 20:03:26 +0200

    Update codons.py

--- codons.py
+++ codons.py
@@ -1,4 +1,12 @@
 def create_codon_dict(file_path):
-    pass # Replace the pass with your code
-
+  file = open(file_path)
+  rows = file.readlines()
+  file.close
+  dic={}
+  for row in rows:
+    row = row.strip().split('\t')
+    codon = row[0]
+    amino_acid = row[2]
+    dic[codon] = amino_acid
+  return dic
 
